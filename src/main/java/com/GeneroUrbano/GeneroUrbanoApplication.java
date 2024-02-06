fd65039f1aaef8f22bb7cff612fa53766558ce26
JuanPaRojas22 <127228551+JuanPaRojas22@users.noreply.github.com>
2024-02-05 20:07:19 -0600

    Finalización Practica semana 3

---
+++ src/main/java/com/GeneroUrbano/GeneroUrbanoApplication.java
@@ -0,0 +1,13 @@
+package com.GeneroUrbano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneroUrbanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneroUrbanoApplication.class, args);
+	}
+
+}
